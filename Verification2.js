ffb0e8eff709ca4785d22534ff671580bfa4c8f9
iamjonasb2 <92796582+iamjonasb2@users.noreply.github.com>
2022-12-30 17:50:14 +0100

    problem gelöst

--- Verification2.js
+++ Verification2.js
@@ -1,7 +1,7 @@
-import * as pg from 'pg';
+import pkg from 'pg';
   
-export default function verificate(ticketid) {
-  const client = new pg.Client({
+export default async function verificate(ticketid) {
+ /* const client = new pg.Client({
     host: 'bq3zrot8kwkjsoiu0e7z-postgresql.services.clever-cloud.com',
     port: '5432',
     user: 'u4bj5mr4jis06pbvlw5t',
@@ -13,5 +13,36 @@ export default function verificate(ticketid) {
         
   client.query('SELECT ticketid FROM tickets WHERE ticketid = ' + ticketid, (err, res) => {
     return res;
+  });*/
+
+  const client = new pkg.Client({
+    host: 'bq3zrot8kwkjsoiu0e7z-postgresql.services.clever-cloud.com',
+    port: '5432',
+    user: 'u4bj5mr4jis06pbvlw5t',
+    password: 'xJzpptRJm9iXMmmuQkzr',
+    database: 'bq3zrot8kwkjsoiu0e7z'
   });
+
+  /*client.connect();
+  let ergebnis = false;
+  client.query('SELECT ticketid FROM tickets WHERE ticketid =' + ticketid, (err, res) => {
+    console.log(res);
+    if (res.rows[0] != null) {
+      ergebnis = true;      
+    } else {
+      ergebnis = false;
+    }
+  }); 
+  console.log("Du Kek:" + ergebnis);
+  return ergebnis;
+  client.end();
+}*/
+await client.connect();
+  let ergebnis = false;
+  const res = await client.query('SELECT ticketid FROM tickets WHERE ticketid =' + ticketid).catch((error)=>{console.log(error)});
+  client.end();
+  if (res.rows[0] != null) {
+    ergebnis = true;      
+  }
+  return ergebnis;
 }
