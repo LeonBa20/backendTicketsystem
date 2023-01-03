de1e5f93c32605e40d229d0856fbc598508b4203
iamjonasb2 <92796582+iamjonasb2@users.noreply.github.com>
2023-01-03 17:35:32 +0100

    bugfixes

--- Services/Userservice.js
+++ Services/Userservice.js
@@ -6,7 +6,7 @@ export class Userservice{
     constructor() {}
 
     async createUserTable() {
-        await dbsql('CREATE TABLE users (user_id SERIAL PRIMARY KEY, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, first_name VARCHAR(255), last_name VARCHAR(255), birthdate DATE, created_at TIMESTAMP DEFAULT NOW(), updated_at TIMESTAMP DEFAULT NOW()');
+        await dbsql('CREATE TABLE users (user_id SERIAL PRIMARY KEY, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, first_name VARCHAR(255), last_name VARCHAR(255), birthdate DATE, created_at TIMESTAMP DEFAULT NOW(), updated_at TIMESTAMP DEFAULT NOW())');
       }
 
     async exampleData(){
